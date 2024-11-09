3baf91a178002a4375fb98e3e474e6add338daae
Kanghee Park <kpark247@wisc.edu>
2024-11-08 18:04:39 -0800

    Implement partial lexer

--- partial_lexer.py
+++ partial_lexer.py
@@ -1,27 +1,86 @@
-from typing import TYPE_CHECKING, Sequence, Tuple
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Any, Iterator, Optional, Tuple
 
-from .partial_parser import ParserToken
+from lark.exceptions import UnexpectedCharacters
+from lark.lexer import (
+    BasicLexer,
+    Lexer,
+    LexerState
+)
 
 if TYPE_CHECKING:
-    pass
+    from lark.lexer import LexerConf, Token
 
-class PartialLexer:
+class PartialLexerState(LexerState):
+    """Represents the current state of the partial lexer"""
+
+    def __init__(self, text: str, last_token: Optional["Token"]=None):
+        super().__init__(text=text, line_ctr=None, last_token=last_token)
+
+    def remainder(self) -> str:
+        """
+        Compute unlexed suffix of the text
+        
+        Return:
+            `str`: unlexed suffix of the text
+        """
+        return self.text[self.line_ctr.char_pos:]
+
+class PartialLexer(Lexer):
     """
     A base class for partial lexer,
     that splits a prefix into a sequence of lexed tokens and unlexed suffix.
     """
 
-    def lex(self, prefix: str) -> Tuple[Sequence[ParserToken], str]:
+    @abstractmethod
+    def partial_lex(
+        self, lexer_state: PartialLexerState, parser_state: Any
+    ) -> Tuple[Iterator["Token"], str]:
         """
         Split a prefix into a sequence of lexed tokens and unlexed suffix
 
         Args:
-            prefix (`str`): an unlexed string
+            lexer_state (`PartialLexerState`): the current state of partial lexer
+            parser_state (`Any`): an optional parser state for contextual lexer
 
-        Return:
-            `Sequence[ParserToken]`: a pair of sequence of lexed tokens
+        Returns:
+            `Iterator[ParserToken]`: a pair of sequence of lexed tokens
             `str`: unlexed suffix
         """
-        raise NotImplementedError(
-            f"{self.__class__} is an abstract class. Only classes inheriting this class can call `lex`."
-        )
+
+class PartialBasicLexer(BasicLexer, PartialLexer):
+    """A non-contextual partial lexer"""
+
+    def __init__(self, conf: "LexerConf"):
+        super().__init__(conf)
+
+    def partial_lex(
+        self, lexer_state: PartialLexerState, parser_state: Any = None
+    ) -> Tuple[Iterator["Token"], str]:
+        """
+        Split a prefix into a sequence of lexed tokens and unlexed suffix
+
+        Args:
+            lexer_state (`PartialLexerState`): the current state of partial lexer
+            parser_state (`Any`): an optional parser state (not used, for inheritance)
+
+        Return:
+            `Iterator[ParserToken]`: a pair of sequence of lexed tokens
+            `str`: unlexed suffix
+        """
+        lexer_tokens = []
+        lexing_incomplete = False
+        try:
+            while lexer_state.line_ctr.char_pos < len(lexer_state.text):
+                token = self.next_token(lexer_state)
+                lexer_tokens.append(token)
+        except UnexpectedCharacters:
+            lexing_incomplete = True
+        except EOFError:
+            pass
+
+        if lexing_incomplete:
+            return lexer_tokens, lexer_state.remainder()
+        else:
+            remainder = lexer_tokens[-1] if len(lexer_tokens) > 0 else ""
+            return lexer_tokens[:-1], remainder
--- partial_parser.py
+++ partial_parser.py
@@ -1,11 +1,7 @@
-from typing import TYPE_CHECKING, Sequence, Set
+from typing import TYPE_CHECKING, Iterator, Set
 
 if TYPE_CHECKING:
-    pass
-
-
-class ParserToken:
-    """A base class for parser token"""
+    from lark.lexer import Token
 
 class PartialParser:
     """
@@ -13,15 +9,15 @@ class PartialParser:
     that parses a sequence of lexed tokens and predicts available next tokens.
     """
 
-    def parse(self, tokens: Sequence[ParserToken]) -> Set[ParserToken]:
+    def parse(self, tokens: Iterator["Token"]) -> Set["Token"]:
         """
         Parse a sequence of lexed tokens and computes acceptable next tokens
 
         Args:
-            tokens (`Sequence[ParserToken]): a sequence of lexed tokens
+            tokens (`Iterator[Token]): a sequence of lexed tokens
 
         Return:
-            `Set[ParserToken]`: a set of acceptable next tokens
+            `Set[Token]`: a set of acceptable next tokens
         """
         raise NotImplementedError(
             f"{self.__class__} is an abstract class. Only classes inheriting this class can call `parse`."
